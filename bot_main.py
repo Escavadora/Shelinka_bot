5b8b4947edb130c06af5807d3cc25d3c50d0cb87
David Grölle <david.groelle@protonmail.com>
2018-07-24 21:51:19 +0200

    a commit

--- bot_main.py
+++ bot_main.py
@@ -2,42 +2,57 @@
 # TODO regex for FAQ
 
 import discord
+import asyncio
 from discord.utils import get
+from discord.ext import commands
 
 tokenFile = open('token.txt', 'r')
 TOKEN = tokenFile.read()
+bot = commands.Bot(command_prefix='!', description='Unicorn Petting Zoo 2019')
 
-client = discord.Client()
+async def isSecretUser(ctx):
+    return ctx.author.id == 204287692542836736
 
-@client.event
-async def on_message(message):
-
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!faq'):
-        msg = 'Hey there :wave: , please read #roles and #faq , you will find almost everything there. If you have any other questions feel free to ask :CuteBirb: '.format(message)
-        await client.send_message(message.channel, msg)
-
-    if message.content.startswith('!shelinka'):
-        if message.author.id == ('204287692542836736'):
-            msg = 'https://i.imgur.com/Q4PtRPr.jpg'.format(message)
-            await client.send_message(message.channel, msg)
-
-    if message.content.startswith(''):
-        if message.channel == client.get_channel('468781121497333762'):
-            emoji1 = get(client.get_all_emojis(), name='Ready')
-            emoji2 = get(client.get_all_emojis(), name='NotReady')
-            await client.add_reaction(message, emoji1)
-            await client.add_reaction(message, emoji2)
-
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name='Unicorn Petting Zoo 2019'))
+
+
+@bot.listen()
+async def reactMessage(message):
+    if message.channel == client.get_channel('468781121497333762'):
+        emoji1 = get(client.get_all_emojis(), name='Ready')
+        emoji2 = get(client.get_all_emojis(), name='NotReady')
+        await message.add_reaction(emoji1)
+        await message.add_reaction(emoji2)
+
+
+@bot.command()
+async def faq(ctx, arg):
+    emojibirb = get(bot.get_all_emojis(), name='CuteBirb')
+    msg = 'Hey there :wave: , please read #roles and #faq , you will find almost everything there. If you have any other questions feel free to ask {} '.format(emojibirb)
+    await bot.say(msg)
+
+@bot.command()
+@commands.check(isSecretUser)
+async def shelinka():
+    if message.author.id != '204287692542836736':
+        return
+    msg = 'https://i.imgur.com/Q4PtRPr.jpg'.format(message)
+    await bot.say(msg)
+
+@bot.command()
+async def list():
+    bot.say("Starting to look for wrong roles")
+    mem = message.server.members
+    acceptableroles = ["High Warlord", "Scout", "Centurion", "Spellwing Champion", "Peon", "Event General"]
+    for member in mem:
+        if not any((x.name in acceptableroles) for x in member.roles):
+            await client.send_message(message.channel, member.mention)
+    await client.send_message(message.channel, "Done looking for wrong roles")
+
 
 client.run(TOKEN)
