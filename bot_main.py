9babe3f12f6d1423add47be603c4523aa5fe2e40
Escavadora <patrik.bagar@gmail.com>
2018-07-27 12:52:06 +0200

    Fixed bot and !faq has cooldown now

--- bot_main.py
+++ bot_main.py
@@ -1,27 +1,39 @@
 # https://github.com/Escavadora/Shelinka_bot
-# TODO regex for FAQ
+# TODO add description='Unicorn Petting Zoo 2019' somewhere
 
 import discord
 import asyncio
 from discord.utils import get
 from discord.ext import commands
+from discord.ext.commands import Bot
 
-tokenFile = open('token.txt', 'r')
+# Comment this line if you want to test
+# tokenFile = open('token.txt', 'r')
+
+# Comment this line if you want to deploy
+tokenFile = open('token2.txt', 'r')
+
+BOT_PREFIX = ('!', '?')
 TOKEN = tokenFile.read()
-bot = commands.Bot(command_prefix='!', description='Unicorn Petting Zoo 2019')
+client = Bot(command_prefix=BOT_PREFIX)
+
+def __init__(self, client):
+    self.client = client
+
 
 async def isSecretUser(ctx):
     return ctx.author.id == 204287692542836736
 
-@bot.event
+
+@client.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(client.user.name)
+    print(client.user.id)
     print('------')
 
 
-@bot.listen()
+@client.listen()
 async def reactMessage(message):
     if message.channel == client.get_channel('468781121497333762'):
         emoji1 = get(client.get_all_emojis(), name='Ready')
@@ -30,21 +42,23 @@ async def reactMessage(message):
         await message.add_reaction(emoji2)
 
 
-@bot.command()
-async def faq(ctx, arg):
-    emojibirb = get(bot.get_all_emojis(), name='CuteBirb')
-    msg = 'Hey there :wave: , please read #roles and #faq , you will find almost everything there. If you have any other questions feel free to ask {} '.format(emojibirb)
-    await bot.say(msg)
+@client.command()
+@commands.cooldown(1, 60, commands.BucketType.server)
+async def faq():
+    # emojibirb = get(bot.get_all_emojis(), name='CuteBirb')
+    msg = 'Hey there :wave: , please read #roles and #faq , you will find almost everything there. ' \
+          'If you have any other questions feel free to ask <:CuteBirb:454812443752005632> '
+    await client.say(msg)
 
-@bot.command()
+'''@bot.command()
 @commands.check(isSecretUser)
 async def shelinka():
     if message.author.id != '204287692542836736':
         return
     msg = 'https://i.imgur.com/Q4PtRPr.jpg'.format(message)
-    await bot.say(msg)
+    await bot.say(msg)'''
 
-@bot.command()
+'''@bot.command()
 async def list():
     bot.say("Starting to look for wrong roles")
     mem = message.server.members
@@ -52,7 +66,7 @@ async def list():
     for member in mem:
         if not any((x.name in acceptableroles) for x in member.roles):
             await client.send_message(message.channel, member.mention)
-    await client.send_message(message.channel, "Done looking for wrong roles")
+    await client.send_message(message.channel, "Done looking for wrong roles")'''
 
 
 client.run(TOKEN)
