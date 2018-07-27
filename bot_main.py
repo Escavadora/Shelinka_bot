51641c808234f0f978d191fa30d6b66b6e6fa897
Escavadora <patrik.bagar@gmail.com>
2018-07-27 16:28:24 +0200

    Made some changes to !raffle, not working atm

--- bot_main.py
+++ bot_main.py
@@ -1,9 +1,12 @@
 # https://github.com/Escavadora/Shelinka_bot
+# TODO reroll
 
 from discord.utils import get
 from discord.ext import commands
 from discord.ext.commands import Bot
 from discord import Game
+import datetime
+import discord
 
 # Comment this line if you want to test
 # tokenFile = open('token.txt', 'r')
@@ -32,6 +35,7 @@ async def on_message(message):
         emoji2 = get(client.get_all_emojis(), name='NotReady')
         await client.add_reaction(message, emoji1)
         await client.add_reaction(message, emoji2)
+    await client.process_commands(message)
 
 
 @client.command(description='Posts response leading people to #faq and #roles')
@@ -45,6 +49,7 @@ async def faq():
 @client.command(brief='Meme ?',
                 aliases=['Shelinka'],
                 pass_context=True)
+@commands.cooldown(1, 300, commands.BucketType.server)
 async def shelinka(ctx):
     if ctx.message.author.id != '204287692542836736':
         return
@@ -52,15 +57,34 @@ async def shelinka(ctx):
         msg = 'https://i.imgur.com/Q4PtRPr.jpg'
         await client.say(msg)
 
-'''@bot.command()
+
+'''@client.command(brief='Raffle command',
+                description='Use command in format !raffle <raffle name> <channel> <duration> <number of winners>',
+                pass_context=True)
+async def raffle(mesg, channel, duration, winner_num, *, name):
+    embed = discord.Embed(
+        title=name,
+        description=f'React with :tada: to win!',
+        color=discord.Color.magenta(),
+        #timestamp='19:10'
+    )
+    #embed.set_footer(text='Ends at')
+    msg = await channel.send(embed=embed)
+    await msg.add_reaction(':tada:')'''
+'''msg = ':point_right: **GIVEAWAY** :point_left:\n*{}*\n**{} WINNERS** \nReact with :tada: to enter!' \
+            '\nDuration: {} seconds\nEnds at: {}'.format(name, winner_num, duration, channel)
+    await client.say(msg)
+    await client.add_reaction(msg, 'tada')'''
+
+'''@bot.command(pass_context=True)
 async def list():
-    bot.say("Starting to look for wrong roles")
+    client.say("Starting to look for wrong roles")
     mem = message.server.members
     acceptableroles = ["High Warlord", "Scout", "Centurion", "Spellwing Champion", "Peon", "Event General"]
     for member in mem:
         if not any((x.name in acceptableroles) for x in member.roles):
             await client.send_message(message.channel, member.mention)
-    await client.send_message(message.channel, "Done looking for wrong roles")'''
+    await client.say(message.channel, "Done looking for wrong roles")'''
 
 
 client.run(TOKEN)
